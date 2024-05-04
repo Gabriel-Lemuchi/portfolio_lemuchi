538a009b6345a12bfc25286fc657cf5fe53ca4a1
Gabriel Lemuchi Braz <118304199+Gabriel-Lemuchi@users.noreply.github.com>
2024-05-04 12:26:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const body = document.querySelector('body')
 const chk = document.querySelector('#chk')
 const header = document.querySelector('header')
+const footer = document.querySelector('footer')
 
 chk.addEventListener("click", () =>{
-    if(!body.classList.contains("dark") && !header.classList.contains("dark")){
+    if(!body.classList.contains("dark") && !header.classList.contains("dark") && !footer.classList.contains("dark")){
         body.classList.add("dark")
         header.classList.add("dark")
+        footer.classList.add("dark")
     }
     else{
         body.classList.remove("dark")
         header.classList.remove("dark")
+        footer.classList.remove("dark")
     }
 })
